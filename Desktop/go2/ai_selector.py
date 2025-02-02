746ba032ec1ce8fa4e29dccaad6aa764e7d6ecbf
swjin306@gatech.edu <jinsw90@gmail.com>
2025-02-02 06:22:41 -0500

    post-processed assistant

--- Desktop/go2/ai_selector.py
+++
@@ -1,7 +0,0 @@
-import openai_client
-
-class AiSelector:
-    @staticmethod
-    def getClient(env: str, key):
-        if env["ai"] == "openai":
-            return openai_client.OpenaiClient(env, key)
--- Desktop/go2/round.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from ai_client_base import ResponseMsg
-
-@dataclass
-class Round:
-    round_number: int
-    detected_objects: list
-    chat: list
-    assistant: ResponseMsg
